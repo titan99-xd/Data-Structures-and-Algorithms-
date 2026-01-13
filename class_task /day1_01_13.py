f7556587c3441e379610aacf8658a473a3caaa72
titan99-xd <abhinavgautam3166@gmail.com>
2026-01-13 14:34:27 +0200

    first commint

---
+++ class_task /day1_01_13.py
@@ -0,0 +1,102 @@
+# ch 1
+
+# a = 5
+# b = 6
+# c = a + b
+# print(c)
+# print("Hello, World!")
+
+
+# a = [1, 2, 3, 4, 5]
+# b = a
+# print(b)
+
+
+# variable part 2
+
+# we can have multiplie different types
+# list_var = ['a', 'b', 10, 20, True, False]
+# tuple_var = ('a', 'b', 10, 20, True)  # mmutable remember parentheses
+# string_var = "Amazing"  # string is also a list but can contain ONLY characters
+
+# print(string_var[-1])
+
+# indexing
+
+# for character in string_var:
+#     print(character)
+
+
+# dictionary
+# dict_var = {
+#     'name': 'john',
+#     'surname': 'doe',
+#     'age': 30,
+#     'has_car': True
+# }
+
+# print(dict_var)
+
+
+# for obj in dict_var:
+#     print(obj)
+
+
+# some_var = 3
+# if some_var == 1:
+#     print('it is ', some_var)
+# elif some_var ==2:
+#     print('it is ',some_var)
+# else:
+#     print('it is something else')
+
+
+# for i in range(5):
+#     print(i)
+
+
+# string_var = "amazon"
+# for char in string_var:
+#     print(char)
+
+
+# while loop
+# a = 0
+# while a < 10:
+#     b= int(input('give me a number: '))
+
+#     if b == 5:
+#         print("b was 5 so we break")
+#         break
+#     a += 5
+
+
+# def amazingFunction( isTrue: bool):
+#     if isTrue == True:
+#         print("this is amazing! wow!")
+#     else:
+#         print('this sucks xD')
+
+
+# amazingFunction(False)
+
+
+# classes and object
+
+
+# class
+
+# class HumanBeing():
+#     def __init__(self, height, weight, age):
+#         self.height = height
+#         self.weight = weight
+#         self.age = age
+
+# # class function
+#     def bodyMassIndex(self):
+#         return self.weight / self. height
+
+
+# human1 = HumanBeing(200, 100, 30)
+
+# print(human1.bodyMassIndex())
